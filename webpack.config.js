3fd0371f04590873392d6d24676b4f203b7c9319
Toby Hsieh <thrasher13b@gmail.com>
2020-12-11 14:02:48 -0800

    Change webpack devtool option to none
    The eval devtool option (the default for development) does not work
    because Chrome extensions don't allow eval by default.

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 const config = {
   mode: process.env.NODE_ENV || 'development',
+  devtool: 'none',
   entry: {
     options: './src/options.js',
     background: './src/background/main.js',
